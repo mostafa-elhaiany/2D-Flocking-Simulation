0614be194778c881771e68aa1052c711b9a1484d
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2024-06-22 22:23:42 +0200

    added cohesion

--- Boid.py
+++ Boid.py
@@ -11,12 +11,29 @@ class Boid:
         self.perception_distance = perception_distance
         self.radius = radius
         self.max_force = max_force
+        self.max_speed = 4
 
     def draw(self, screen):
         pygame.draw.circle(screen, (255,255,255), self.position, self.radius)
 
     def distance_to(self, other):
         return self.position.distance_to(other.position)
+
+    def cohesion(self,local_flock):
+        avg = math.Vector2(0,0)
+        if(len(local_flock)!=0):
+            for boid in local_flock:
+                avg += boid.position
+
+            position_direction = ((avg/len(local_flock)) - self.position)
+
+            direction = position_direction - self.velocity
+            if(direction.magnitude!=0):
+                direction.normalize_ip()
+            
+            return direction * self.max_force
+        else:
+            return avg
 
     def align(self, local_flock):
         avg = math.Vector2(0,0)
@@ -26,8 +43,8 @@ class Boid:
 
             direction = ((avg/len(local_flock)) - self.velocity)
             if(direction.x!=0 or direction.y!=0):
-                direction = direction.normalize() * self.max_force
-            return direction
+                direction = direction.normalize() 
+            return direction * self.max_force
         else:
             return avg
 
@@ -35,8 +52,10 @@ class Boid:
     def update(self, local_flock):
 
         align_velocity = self.align(local_flock)
+        cohesion_velocity = self.cohesion(local_flock)
 
-        self.acceleration = align_velocity
+        self.acceleration = align_velocity + cohesion_velocity
 
         self.velocity += self.acceleration 
+        self.velocity = self.velocity.normalize() * self.max_speed
         self.position += self.velocity
